ccea78f45be9abb54ad5246bc26020e206f6fe8e
Pavel <pashokdragon2012@gmail.com>
2022-10-12 21:02:48 +0300

    -добавлены зависимости для базы данных mySql -добавлена model постов -сделан вывод постов из базы данных mySql на странице "Блог"

---
+++ src/main/java/com/lost/blog/models/Post.java
@@ -0,0 +1,58 @@
+package com.lost.blog.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String announcement;
+    private String fullText;
+    private int views;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnnouncement() {
+        return announcement;
+    }
+
+    public void setAnnouncement(String announcement) {
+        this.announcement = announcement;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+}
